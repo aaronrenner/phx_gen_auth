f23cec7480fbd86d4273b4c3b42f69dc2456d0ff
Aaron Renner <aaron.renner@gmail.com>
2020-03-24 14:56:00 -0600

    Add ci

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Phx.Gen.Auth.MixProject do
     [
       app: :phx_gen_auth,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,7 @@ defmodule Phx.Gen.Auth.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix, github: "phoenixframework/phoenix"}
     ]
   end
 end
