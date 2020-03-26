37044eedb0f3a874c4efeff223150f8f0b9a179a
Aaron Renner <aaron.renner@gmail.com>
2020-03-26 14:46:59 -0600

    Fix interpolation for route_helper

--- priv/templates/phx.gen.auth/auth.ex
+++ priv/templates/phx.gen.auth/auth.ex
@@ -124,7 +124,7 @@ defmodule <%= inspect auth_module %> do
       conn
       |> put_flash(:error, "You must login to access this page.")
       |> maybe_store_return_to()
-      |> redirect(to: Routes.<%= schema.singular %>_session_path(conn, :new))
+      |> redirect(to: Routes.<%= schema.route_helper %>_session_path(conn, :new))
       |> halt()
     end
   end
