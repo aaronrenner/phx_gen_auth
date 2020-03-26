4bbf393aaab087aeac8b58b3db963e728f43a9d1
Aaron Renner <aaron.renner@gmail.com>
2020-03-26 14:47:22 -0600

    Generate confirmation controller and tests

--- test/mix/tasks/phx.gen.auth_test.exs
+++ test/mix/tasks/phx.gen.auth_test.exs
@@ -74,6 +74,13 @@ defmodule Mix.Tasks.Phx.Gen.AuthTest do
         assert file =~ "PhxGenAuthWeb.Endpoint.config("
       end)
 
+      assert_file("lib/phx_gen_auth_web/controllers/user_confirmation_controller.ex")
+      assert_file("lib/phx_gen_auth_web/templates/user_confirmation/new.html.eex")
+
+      assert_file("test/phx_gen_auth_web/controllers/user_confirmation_controller_test.exs", fn file ->
+        assert file =~ "defmodule PhxGenAuthWeb.UserConfirmationControllerTest"
+      end)
+
       assert_file("lib/phx_gen_auth_web/views/user_confirmation_view.ex", fn file ->
         assert file =~ "defmodule PhxGenAuthWeb.UserConfirmationView"
       end)
