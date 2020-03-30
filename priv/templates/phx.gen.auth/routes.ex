1437117c7ae7f1ec9a6295f2fc0827b85de3d1e4
Aaron Renner <aaron.renner@gmail.com>
2020-03-30 13:38:47 -0600

    Add additional placeholders to routes template

--- priv/templates/phx.gen.auth/routes.ex
+++ priv/templates/phx.gen.auth/routes.ex
@@ -2,32 +2,32 @@
   ## Authentication routes
 
   scope "/", <%= inspect context.web_module %> do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
+    pipe_through [:browser, :redirect_if_<%= schema.singular %>_is_authenticated]
 
-    get "/users/register", <%= inspect schema.alias %>RegistrationController, :new
-    post "/users/register", <%= inspect schema.alias %>RegistrationController, :create
-    get "/users/login", <%= inspect schema.alias %>SessionController, :new
-    post "/users/login", <%= inspect schema.alias %>SessionController, :create
-    get "/users/reset_password", <%= inspect schema.alias %>ResetPasswordController, :new
-    post "/users/reset_password", <%= inspect schema.alias %>ResetPasswordController, :create
-    get "/users/reset_password/:token", <%= inspect schema.alias %>ResetPasswordController, :edit
-    put "/users/reset_password/:token", <%= inspect schema.alias %>ResetPasswordController, :update
+    get "/<%= schema.plural %>/register", <%= inspect schema.alias %>RegistrationController, :new
+    post "/<%= schema.plural %>/register", <%= inspect schema.alias %>RegistrationController, :create
+    get "/<%= schema.plural %>/login", <%= inspect schema.alias %>SessionController, :new
+    post "/<%= schema.plural %>/login", <%= inspect schema.alias %>SessionController, :create
+    get "/<%= schema.plural %>/reset_password", <%= inspect schema.alias %>ResetPasswordController, :new
+    post "/<%= schema.plural %>/reset_password", <%= inspect schema.alias %>ResetPasswordController, :create
+    get "/<%= schema.plural %>/reset_password/:token", <%= inspect schema.alias %>ResetPasswordController, :edit
+    put "/<%= schema.plural %>/reset_password/:token", <%= inspect schema.alias %>ResetPasswordController, :update
   end
 
   scope "/", <%= inspect context.web_module %> do
-    pipe_through [:browser, :require_authenticated_user]
+    pipe_through [:browser, :require_authenticated_<%= schema.singular %>]
 
-    delete "/users/logout", <%= inspect schema.alias %>SessionController, :delete
-    get "/users/settings", <%= inspect schema.alias %>SettingsController, :edit
-    put "/users/settings/update_password", <%= inspect schema.alias %>SettingsController, :update_password
-    put "/users/settings/update_email", <%= inspect schema.alias %>SettingsController, :update_email
-    get "/users/settings/confirm_email/:token", <%= inspect schema.alias %>SettingsController, :confirm_email
+    delete "/<%= schema.plural %>/logout", <%= inspect schema.alias %>SessionController, :delete
+    get "/<%= schema.plural %>/settings", <%= inspect schema.alias %>SettingsController, :edit
+    put "/<%= schema.plural %>/settings/update_password", <%= inspect schema.alias %>SettingsController, :update_password
+    put "/<%= schema.plural %>/settings/update_email", <%= inspect schema.alias %>SettingsController, :update_email
+    get "/<%= schema.plural %>/settings/confirm_email/:token", <%= inspect schema.alias %>SettingsController, :confirm_email
   end
 
   scope "/", <%= inspect context.web_module %> do
     pipe_through [:browser]
 
-    get "/users/confirm", <%= inspect schema.alias %>ConfirmationController, :new
-    post "/users/confirm", <%= inspect schema.alias %>ConfirmationController, :create
-    get "/users/confirm/:token", <%= inspect schema.alias %>ConfirmationController, :confirm
+    get "/<%= schema.plural %>/confirm", <%= inspect schema.alias %>ConfirmationController, :new
+    post "/<%= schema.plural %>/confirm", <%= inspect schema.alias %>ConfirmationController, :create
+    get "/<%= schema.plural %>/confirm/:token", <%= inspect schema.alias %>ConfirmationController, :confirm
   end
