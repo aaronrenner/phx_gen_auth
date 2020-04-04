0a25932334b7168ecdb9a2c7c3ffad2fd2f2aff3
Aaron Renner <aaron.renner@gmail.com>
2020-04-04 08:20:25 -0600

    Add basic docs

--- lib/mix/tasks/phx.gen.auth.ex
+++ lib/mix/tasks/phx.gen.auth.ex
@@ -5,6 +5,81 @@ defmodule Mix.Tasks.Phx.Gen.Auth do
   Generates authentication logic for a resource
 
       mix phx.gen.auth Accounts User users
+
+  ## Notes about the generated authentication system
+
+  ### Password hashing
+
+  The password hashing mechanism defaults to `bcrypt` for
+  Unix systems and `pdkdf2` for Windows systems. Both
+  systems use [the Comeonin interface](https://hexdocs.pm/comeonin/).
+
+  ### Forbidding access
+
+  The generated code ships with an auth module with handful
+  plugs that fetch the current account, requires authentication
+  and so on. For instance, for an app named Demo which invoked
+  `mix phx.gen.auth Accounts User users`, you will find a module
+  named `DemoWeb.UserAuth` with plugs such as:
+
+    * `fetch_current_user` - fetches the current user information if
+      available
+
+    * `require_authenticated_user` - must be invoked after
+      `fetch_current_user` and requires that a current exists and is
+      authenticated
+
+    * `redirect_if_user_is_not_authenticated` - used for the few
+      pages that must not be available to authenticated users
+
+  ### Confirmation
+
+  The generated functionality ships with an account confirmation
+  mechanism, where users have to confirm their account, typically
+  by e-mail. However, the generated code does not forbid users
+  from using the application if their accounts have not yet been
+  confirmed. You can trivially add this functionality by customizing
+  the plugs generated in the Auth module.
+
+  ### Notifiers
+
+  The generated code is not integrated with any system to send
+  SMSs or e-mails for confirming accounts, reseting passwords,
+  etc. Instead it simply logs a message to the terminal. It is
+  your responsibility to integrate with the proper system after
+  generation.
+
+  ### Tracking sessions
+
+  All sessions and tokens are tracked in a separate table. This
+  allows you to track how many sessions are active for each account.
+  You could even expose this information to users if desired.
+
+  Note that whenever the password changes (either via reset password
+  or directly), all tokens are deleted and the user has to login
+  again on all devices.
+
+  ### Enumeration attacks
+
+  An enumeration attack allows an attacker to enumerate all e-mails
+  registered in the application. The generated authentication code
+  protect against enumeration attacks on all endpoints, except in
+  the registration and update e-mail forms. If your application is
+  really sensitive to enumeration attacks, you need to implement
+  your own registration workflow, which tends to be very different
+  from the workflow for most applications.
+
+  ### Case sensitiveness
+
+  The e-mail lookup is made to be case insensitive. Case insensitive
+  lookups are the default in MySQL and MSSQL but require the
+  citext extension in Postgres.
+
+  ### Concurrent tests
+
+  The generated tests run concurrently if you are using a database
+  that supports concurrent tests (Postgres or MSSQL with snapshot
+  isolation level).
   """
 
   use Mix.Task
@@ -23,7 +98,10 @@ defmodule Mix.Tasks.Phx.Gen.Auth do
     Gen.Context.prompt_for_code_injection(context)
 
     # Needed so we can get the ecto adapter and ensure other
-    # libraries are loaded
+    # libraries are loaded.
+    #
+    # As far as I can tell, everything after this must be tested with an
+    # integration test.
     Mix.Task.run("compile")
 
     validate_required_dependencies!()
