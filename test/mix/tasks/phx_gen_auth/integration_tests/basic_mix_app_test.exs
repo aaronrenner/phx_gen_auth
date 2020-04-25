e2425fa3dc5612e0e4a76cb9991500e803b6d7d0
Aaron Renner <aaron.renner@gmail.com>
2020-04-24 21:52:05 -0600

    Remove extra todos

--- test/mix/tasks/phx_gen_auth/integration_tests/basic_mix_app_test.exs
+++ test/mix/tasks/phx_gen_auth/integration_tests/basic_mix_app_test.exs
@@ -15,7 +15,6 @@ defmodule Phx.Gen.Auth.IntegrationTests.BasicMixAppTest do
 
   test "errors in basic mix project", %{test_app_path: test_app_path} do
     {output, 1} = mix_run(~w(phx.gen.auth Accounts User users), cd: test_app_path)
-    # TODO: figure out how to return a better error here
     assert output =~ "mix phx.gen.auth requires ecto_sql"
   end
 end
--- test/mix/tasks/phx_gen_auth/integration_tests/no_ecto_app_test.exs
+++ test/mix/tasks/phx_gen_auth/integration_tests/no_ecto_app_test.exs
@@ -15,7 +15,6 @@ defmodule Phx.Gen.Auth.IntegrationTests.NoEctoAppTest do
 
   test "errors in phoenix project with --no-ecto", %{test_app_path: test_app_path} do
     assert {output, 1} = mix_run(~w(phx.gen.auth Accounts User users), cd: test_app_path)
-    # TODO: come up with a better error here
     assert output =~ "mix phx.gen.auth requires ecto_sql"
   end
 end
