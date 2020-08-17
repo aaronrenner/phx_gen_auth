809a007766ffb355727057cabb1a75d4a526dc8e
Matthew Lehner <matthew@mpl.io>
2020-08-17 12:34:44 -0700

    Update maybe_store_return_to to handle query string (#68)
    Co-authored-by: José Valim <jose.valim@gmail.com>

--- priv/templates/phx.gen.auth/auth_test.exs
+++ priv/templates/phx.gen.auth/auth_test.exs
@@ -138,12 +138,20 @@ defmodule <%= inspect auth_module %>Test do
 
     test "stores the path to redirect to on GET", %{conn: conn} do
       halted_conn =
-        %{conn | request_path: "/foo?bar"}
+        %{conn | request_path: "/foo", query_string: ""}
         |> fetch_flash()
         |> <%= inspect schema.alias %>Auth.require_authenticated_<%= schema.singular %>([])
 
       assert halted_conn.halted
-      assert get_session(halted_conn, :<%= schema.singular %>_return_to) == "/foo?bar"
+      assert get_session(halted_conn, :<%= schema.singular %>_return_to) == "/foo"
+
+      halted_conn =
+        %{conn | request_path: "/foo", query_string: "bar=baz"}
+        |> fetch_flash()
+        |> <%= inspect schema.alias %>Auth.require_authenticated_<%= schema.singular %>([])
+
+      assert halted_conn.halted
+      assert get_session(halted_conn, :<%= schema.singular %>_return_to) == "/foo?bar=baz"
 
       halted_conn =
         %{conn | request_path: "/foo?bar", method: "POST"}
